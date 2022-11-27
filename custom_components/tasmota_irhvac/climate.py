bb2862aa0872558b3bcb4b6022fc551311d5135a
PostLogical <robby.griffin@gmail.com>
2022-11-27 11:45:42 -0500

    Logging additions to fix reboot loss of state.

--- custom_components/tasmota_irhvac/climate.py
+++ custom_components/tasmota_irhvac/climate.py
@@ -535,6 +535,7 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity, MqttAvailability):
         # Check If we have an old state
         old_state = await self.async_get_last_state()
         if old_state is not None:
+            _Logger.warning("There was an old state, so trying to restore.")
             # If we have no initial temperature, restore
             if old_state.attributes.get(ATTR_TEMPERATURE) is not None:
                 self._target_temp = float(
@@ -569,6 +570,7 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity, MqttAvailability):
                 self._fix_swingh = self._swingh
         else:
             # No previous state, try and restore defaults
+            _Logger.warning("No old state, so using defaults.")
             if self._target_temp is None:
                 self._target_temp = self._def_target_temp
             _LOGGER.warning(
