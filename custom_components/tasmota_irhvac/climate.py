338b62649fcc550bb0213cb9491a8a3925d31e0a
PostLogical <robby.griffin@gmail.com>
2022-11-26 19:03:41 -0500

    Try retaining mqtt

--- custom_components/tasmota_irhvac/climate.py
+++ custom_components/tasmota_irhvac/climate.py
@@ -778,6 +778,7 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity, MqttAvailability):
                         "topic": self.state_topic,
                         "msg_callback": state_message_received,
                         "qos": 1,
+                        "retain": true,
                     }
                 }
 
